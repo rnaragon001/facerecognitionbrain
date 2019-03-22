88d5269e7920869d5a142180dece54ebfc863f0f
Rod Aragon <rnaragon001@yahoo.com>
2019-03-23 01:01:29 +0800

    Updates

---
+++ precache-manifest.7ad7ef7f7a1fffef17a6196ea7a5c359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9fa71fd449ffc1f9a70",
+    "url": "/facerecognitionbrain/static/js/runtime~main.a9fa71fd.js"
+  },
+  {
+    "revision": "be0e565785ece7332f9e",
+    "url": "/facerecognitionbrain/static/js/main.be0e5657.chunk.js"
+  },
+  {
+    "revision": "ea06ff8a949131482810",
+    "url": "/facerecognitionbrain/static/js/2.ea06ff8a.chunk.js"
+  },
+  {
+    "revision": "be0e565785ece7332f9e",
+    "url": "/facerecognitionbrain/static/css/main.b75b67da.chunk.css"
+  },
+  {
+    "revision": "ea06ff8a949131482810",
+    "url": "/facerecognitionbrain/static/css/2.a59c5e52.chunk.css"
+  },
+  {
+    "revision": "4666037b540377916e7f83ed3cfa0289",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
